6e54466a30caf0f053abdbf132b4b620e92b4208
alishafaizan11 <alishafaizan@gmail.com>
2024-02-01 15:00:47 +0400

    Feature B added

--- lab1git/src/lab1git/example.java
+++ lab1git/src/lab1git/example.java
@@ -4,7 +4,8 @@ public class example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World");	
+		System.out.println("Hello World");	
+		System.out.println("Message from Member B: FeatureB");
 		}
 
 }
